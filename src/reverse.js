6c402856d75303993bfb75ace2f1d090525e0c82
John Giglio <JGiglio@ucvts.tec.nj.us>
2019-11-15 10:52:32 -0500

    completed exercise 2

---
+++ src/reverse.js
@@ -0,0 +1,24 @@
+const readlineSync = require("readline-sync");
+
+let positiveInteger = 0;
+let stringSegment = "";
+let stringFinal = "";
+
+console.log("\n")
+
+do {
+  positiveInteger = Number(readlineSync.question("Positive integer: "));
+} while (!Number.isInteger(positiveInteger) || positiveInteger > Number.MAX_SAFE_INTEGER || positiveInteger < Number.MIN_SAFE_INTEGER || positiveInteger < 0)
+
+while (positiveInteger != 0){
+  stringSegment = positiveInteger % 10;
+  positiveInteger = Math.floor(positiveInteger / 10)
+  if (positiveInteger != 0){
+    stringSegment = stringSegment + ", "
+  } else {
+    stringSegment = stringSegment + "."
+  }
+  stringFinal = stringFinal + stringSegment;
+}
+
+console.log("\n" + stringFinal);
--- src/sum.js
+++ src/sum.js
@@ -9,9 +9,13 @@ do {
   upperBound = Number(readlineSync.question("Upper bound: "));
 } while ((lowerBound > upperBound) || !(Number.isInteger(lowerBound)) || !(Number.isInteger(upperBound)) || (lowerBound > Number.MAX_SAFE_INTEGER) || (upperBound > Number.MAX_SAFE_INTEGER) || (lowerBound < Number.MIN_SAFE_INTEGER) || (upperBound < Number.MIN_SAFE_INTEGER))
 
-let sum = lowerBound;
-
-while (lowerBound <= upperBound){
-  sum = sum + lowerBound++;
+if (lowerBound % 2 != 0) {
+  ++lowerBound
 }
+let sum = 0
+for (let i = sum; lowerBound <= upperBound; lowerBound = lowerBound + 2){
+  i = i + lowerBound;
+  sum = i
+}
+sum = sum.toLocaleString("en", { minimumFractionDigits: 0, maximumFractionDigits: 0 });
 console.log(("\n" + sum + "."))
