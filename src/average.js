57ae85492579bcf096fd108ff0e4d380e0cc17e3
John Giglio <JGiglio@ucvts.tec.nj.us>
2019-11-19 10:52:39 -0500

    fixed some invalid things

--- src/average.js
+++ src/average.js
@@ -8,14 +8,17 @@ let doRepeat = 0;
 console.log("\n")
 
 do {
-  input = Number(readlineSync.question("Non-negative integer: "));
-  if (input >= 0) {
-    sum = sum + input;
-    ++divisor
-  } else {
-    doRepeat = 1;
+  input = (readlineSync.question("Non-negative integer: "));
+  if (Number.isNaN(input) && Number.isInteger(input)) {
+    if (input >= 0) {
+      sum = sum + input;
+      ++divisor
+    } else if (input < 0) {
+      doRepeat = 1;
+    }
   }
-} while (!Number.isInteger(input) || input > Number.MAX_SAFE_INTEGER || doRepeat = 0)
+  console.log(doRepeat)
+} while (doRepeat == 0)
 
 let average = sum / divisor;
 
