11fcaca5f93af2b5c8752f580c5f628740edfb30
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-11-20 21:18:08 -0500

    initial commit, ask Ryan about what to do for 1

---
+++ src/prime.js
@@ -0,0 +1,24 @@
+const readlineSync = require("readline-sync");
+
+let input = 0;
+let sum = 0;
+let divisor = 2;
+let doRepeat = 0;
+let message = "\nPrime."
+
+console.log("\n")
+
+do {
+  input = Number(readlineSync.question("Non-negative integer: "));
+} while (!Number.isInteger(input) || input > Number.MAX_SAFE_INTEGER || input < 0)
+
+while (divisor != input && doRepeat == 0) {
+  if (input % divisor == 0) {
+    doRepeat = 1
+    message = "\nNot prime."
+  } else {
+    ++divisor
+  }
+}
+
+console.log(message)
