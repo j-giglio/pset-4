31377804b0797bec01413f4b88a6e607be2c7f25
j-giglio <55398564+j-giglio@users.noreply.github.com>
2019-11-24 14:35:01 -0500

    complete

--- src/factors.js
+++ src/factors.js
@@ -1,26 +1,38 @@
 const readlineSync = require("readline-sync");
 
 let input = 0;
-let sum = 0;
-let divisor = 2;
-let doRepeat = 0;
-let stringFinal = "."
-let stringSegment = ""
+let divisor = 1;
+let stringFinal = "";
+let firstStringSegment = "";
+let secondStringSegment = ""
 
 console.log("\n")
 
 do {
   input = Number(readlineSync.question("Positive integer: "));
-} while (!Number.isInteger(input) || input > Number.MAX_SAFE_INTEGER || input >= 0)
+} while (!Number.isInteger(input) || input > Number.MAX_SAFE_INTEGER || input <= 0)
 
-while (divisor != input) {
-  if (input % divisor == 0 || input == 1) {
-    stringSegment = divisor
-      if (stringFinal != "."){
-      stringSegment = stringSegment + ", "
-      }
-   }
-    ++divisor
+while (divisor <= Math.sqrt(input)) {
+  if (input % divisor === 0) {
+    firstStringSegment = divisor;
+    secondStringSegment = input / divisor;
+    
+    ++divisor;
+
+    if (firstStringSegment === secondStringSegment) {
+      firstStringSegment = "";
+    } else {
+      firstStringSegment = firstStringSegment + ", ";
+    }
+
+    if (stringFinal != ""){
+      stringFinal = stringFinal + ", "
+    }
+    
+    stringFinal = stringFinal + firstStringSegment + secondStringSegment;
+  } else {
+    ++divisor;
+  }
 }
 
-console.log(message)
+console.log("\n" + stringFinal + ".\n")
